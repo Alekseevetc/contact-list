1baa59bf5c04a7b6444d7aca6a17a38588ff4e44
Александр Алексеевец <alekseevec@MacBook-Air-Aleksandr.local>
2020-10-22 19:56:48 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment, Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Include components
+import ContactList from "./Components/ContactList/ContactList";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  state = {
+    List: [
+      {
+        id: 1,
+        name: "Mila Kunis",
+        role: "Member",
+        avatar: "https://bootdey.com/img/Content/avatar/avatar1.png",
+        created: "2013/08/08",
+        status: "Inactive",
+        email: "mila@kunis.com",
+      },
+      {
+        id: 2,
+        name: "George Clooney",
+        role: "Admin",
+        avatar: "https://bootdey.com/img/Content/avatar/avatar2.png",
+        created: "2013/08/08",
+        status: "Active",
+        email: "marlon@brando.com",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="main-box clearfix">
+              <ContactList List={this.state.List} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
